feb8c364397a7b2d664e1f8f62fca277bbfd4d4e
kharchenkok <k.v.kharchenko@ukr.net>
2020-07-06 15:44:43 +0300

    commit

---
+++ module07/task02/index.js
@@ -0,0 +1,24 @@
+// В HTML есть пустой список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+// В JS есть массив строк.
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+]
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+// после чего вставит все li за одну операцию в список ul.ingredients. 
+// Для создания DOM-узлов используй document.createElement().
+const list = document.querySelector('#ingredients')
+let fragment = document.createDocumentFragment()
+ingredients.forEach(ingredient =>{
+const list = document.createElement('li')
+list.textContent=ingredient
+fragment.append(list)
+})
+list.append(fragment)
