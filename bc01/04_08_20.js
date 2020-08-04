d0504fa0ce1f7fb259d2fd89691ddbdf1dd47943
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-04 17:41:38 +0300

    commit

---
+++ bc01/04_08_20.js
@@ -0,0 +1,73 @@
+//   Напишите скрипт который реализует следующий функционал.
+
+//   Есть кнопка с классом button, текст которой отображает
+//   кол-во раз которое по ней кликнули, обновляется при каждом клике.
+
+// const button = document.querySelector('.button')
+// console.log(button);
+// let numberClick= 0
+// let showNumbOfClick = function(event){
+//     numberClick +=1;
+//     button.textContent = numberClick
+
+// }
+// button.addEventListener('click', showNumbOfClick)
+// ============================================================================
+
+// Даны 2 инпута, абзац и кнопка. По нажатию на кнопку
+//   получите числа которые бьудут введены в инпуты и запишите их сумму в span.result.
+// const body = document.querySelector("body");
+// const button = document.querySelector(".button");
+// const input1 = document.querySelector("#input1");
+// const input2 = document.querySelector("#input2");
+
+// let getResult = function () {
+//     let totalSum = 0;
+//   totalSum = Number(input1.value) + Number(input2.value);
+
+//   body.insertAdjacentHTML("beforeend", `<span>${totalSum}</span>`);
+// clearInput()
+// };
+
+// let clearInput = function(){
+// input1.value = '';
+// input2.value = '';
+// body.querySelector('span').textContent = 'ok';
+
+// }
+
+// button.addEventListener("click", getResult);
+
+// Есть форма с набором радиокнопок. Пользователь выбирает вариант ответа,
+// после чего нажимает кнопку "Submit" и происходит отправка формы.
+
+// При отправке формы:
+//   - не должна перезагружаться страница
+//   - необходимо получить выбранную опцию и вывести в параграф с классом .result
+
+const inputForm = document.querySelector(".input-form");
+const inputWrapper = inputForm.querySelector(".input-wrapper");
+const inputArr = inputWrapper.querySelectorAll("input");
+const buttonWrapper = inputForm.querySelector(".button-wrapper");
+const btnSubmit = inputForm.querySelector(".btnSubmit");
+console.log(inputForm);
+console.log(inputWrapper);
+console.log(inputArr);
+console.log(buttonWrapper);
+console.log(btnSubmit);
+
+let checkInputForm = function (e) {
+  e.preventDefault();
+  inputArr.forEach((input) => {
+    if (input.checked) {
+      buttonWrapper.insertAdjacentHTML(
+        "beforeend",
+        `<p>Ви обрали${input.value}</p>`
+      );
+    } else {
+      alert("Ви нічого не обрали");
+    }
+  });
+};
+
+btnSubmit.addEventListener('submit', checkInputForm);
